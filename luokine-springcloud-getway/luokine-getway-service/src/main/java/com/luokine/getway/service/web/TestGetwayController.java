251619e400850e505798d67da9b102c47ad2361c
bp-tianzhiquan <767050075@qq.com>
2019-09-22 15:14:11 +0800

    修改mapping路径

--- luokine-springcloud-getway/luokine-getway-service/src/main/java/com/luokine/getway/service/web/TestGetwayController.java
+++ luokine-springcloud-getway/luokine-getway-service/src/main/java/com/luokine/getway/service/web/TestGetwayController.java
@@ -2,6 +2,7 @@ package com.luokine.getway.service.web;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,19 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2019-07-05 17:57
  */
 @RestController
+@RequestMapping("/getway")
 public class TestGetwayController {
 
     @Value("${server.port}")
     private String port;
 
-    @GetMapping("/web/getway")
+    @GetMapping("/getPort")
     private String testGetWay(){
         return "getway: port:"+port;
     }
 
-    @GetMapping("/web/getway2")
-    private String testGetWay2(){
-        return "getway22: port:"+port;
-    }
 
 }
