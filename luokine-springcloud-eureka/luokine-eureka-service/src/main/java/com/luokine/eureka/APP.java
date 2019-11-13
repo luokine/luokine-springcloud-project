0875840bd09bbf217e74db9bc1bf2eec37540f45
tianziquan <767050075@qq.com>
2019-11-13 14:39:07 +0800

    update eureka run files

--- luokine-springcloud-eureka/luokine-eureka-service/src/main/java/com/luokine/eureka/APP.java
+++ luokine-springcloud-eureka/luokine-eureka-service/src/main/java/com/luokine/eureka/APP.java
@@ -8,6 +8,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class App {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
